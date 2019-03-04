e8787e8fc99d26e7180d0c6299b0bb5c5623cef5
HangxueLiu <a1102134015@qq.com>
2019-03-04 21:08:29 +0800

    作业二

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        public  void PrintHello()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld hello=new HelloWorld();
+            hello.PrintHello();
+            Console.ReadKey();
+        }
+    }
+}
