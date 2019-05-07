039112bdc9e588578ddd5b54084f5cd507132c1d
Hangxue Liu <1102134015@qq.com>
2019-05-07 15:33:41 +0800

    作业四

---
+++ homework4/homework4/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using homework4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        };
+
+        //1. GetAllProducts方法将整个产品列表作为IEnumerable类型返回。 
+        //2. GetProduct方法通过它的ID来查找单个产品。
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
